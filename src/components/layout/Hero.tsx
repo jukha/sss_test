5bc0f40a7adfede1a6023ca8bd092fd4824e3b28
Junaid <junaid.apsc@gmail.com>
2025-07-02 20:26:06 +0500

    refactor remove extra mobile image prop from base hero component

--- src/components/layout/Hero.tsx
+++ src/components/layout/Hero.tsx
@@ -3,6 +3,7 @@ import React, { ReactNode } from 'react';
 import HeroBottomBar from './HeroBottomBar';
 import Container from './Container';
 import { StampIcon, WaveWithBorder } from '../icons';
+import clsx from 'clsx';
 
 type HeroBottomBarVariant = 'homepage' | 'citypage';
 
@@ -12,14 +13,12 @@ type Props = {
   desktopBgImage: StaticImageData;
   hasBottomBar?: boolean;
   hasSticker?: boolean;
-  mobileBgImage: StaticImageData;
   heroBottomVariant?: HeroBottomBarVariant;
 };
 
 const Hero: React.FC<Props> = ({
   children,
   desktopBgImage,
-  mobileBgImage,
   hasBottomBar = true,
   hasSticker = true,
   heroBottomVariant
@@ -36,7 +35,7 @@ const Hero: React.FC<Props> = ({
         className='absolute top-0 w-full h-[70%] lg:bottom-0 lg:top-0 lg:left-0 lg:w-1/2 lg:h-full bg-[linear-gradient(360deg,rgba(27,129,188,0)_0%,#1B81BC_45%)]
          lg:bg-[linear-gradient(269.54deg,rgba(27,129,188,0)_0%,#1B81BC_34%)] -z-10'
       ></div>
-      <Container className='lg:relative'>
+      <Container className='lg:relative max-lg:bg-[#1b81bc] max-lg:shadow-[0px_0px_15px_rgba(27,_129,_188,_0.7)] z-10'>
         <div className='max-w-[1440px] lg:py-[200px] mx-auto'>{children}</div>
         {/* <StampSticker /> */}
         {hasSticker && (
@@ -50,35 +49,49 @@ const Hero: React.FC<Props> = ({
         {hasBottomBar && <HeroBottomBar variant={heroBottomVariant} />}
       </div>
       {/* desktop image */}
-      <Image
-        src={desktopBgImage}
-        quality={80}
-        width={900}
-        alt=''
-        className='relative hidden lg:block z-[-100] lg:absolute lg:top-0 lg:right-0 lg:object-cover w-full h-full lg:w-[75%] lg:-z-20'
-        priority={true}
-      />
+      <div className='relative hidden lg:block z-[-100] lg:absolute lg:top-0 lg:right-0 lg:object-cover w-full h-full lg:w-[75%] lg:-z-20'>
+        <Image
+          src={desktopBgImage}
+          sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 100vw'
+          fill
+          alt=''
+          className='relative z-[-100] lg:absolute lg:top-0 lg:right-0 lg:object-cover w-full h-full lg:w-[75%] lg:-z-20'
+          priority={true}
+        />
+      </div>
       {!hasBottomBar && (
         <div className='w-full hidden lg:block absolute h-[54px] translate-y-1/3 bottom-0 z-10'>
           <WaveWithBorder />
         </div>
       )}
 
-      <div className='relative lg:hidden -translate-y-24 z-[-100]'>
+      <div
+        className={clsx(
+          'relative lg:hidden z-[-100]',
+          hasBottomBar ? '-translate-y-24' : '-translate-y-10'
+        )}
+      >
         {!hasBottomBar && (
           <div className='w-full absolute h-[54px] -bottom-[26px] z-10'>
             <WaveWithBorder />
           </div>
         )}
 
-        {/* mobile image */}
-        <Image
-          src={mobileBgImage}
-          quality={80}
-          width={1000}
-          alt=''
-          className='relative lg:hidden lg:translate-y-0 z-[-100]'
-        />
+       {/* mobile image */}
+        <div className='relative'>
+          <div
+            className='absolute top-0 w-full h-[50%] lg:bottom-0 lg:top-0 lg:left-0 lg:w-1/2 lg:h-full bg-[linear-gradient(360deg,rgba(27,129,188,0)_0%,#1B81BC_45%)]
+            lg:bg-[linear-gradient(269.54deg,rgba(27,129,188,0)_0%,#1B81BC_34%)] z-[10]'
+          ></div>
+          <Image
+            src={desktopBgImage}
+            quality={80}
+            width={1000}
+            alt=''
+            className='relative lg:hidden lg:translate-y-0 z-[-100]'
+            priority={true}
+          />
+        </div>
       </div>
     </section>
   );
